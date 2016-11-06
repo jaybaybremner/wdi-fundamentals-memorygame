b6c46db278a0b07ecffbe799970ffcc714276236
jaybaybremner <j.bremner@ymail.com>
2016-11-06 12:57:14 +0000

    added js cards

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,15 +1,44 @@
 
-var cardOne = "King"
+/*var cardOne = "King"
 var cardTwo = "King"
 var cardThree = "Queen"
 var cardFour = "Queen"
+var cards = ["King","King,"Queen","Queen"];
 var match = "You have found a match!"
 var diff = "sorry try again"
 
-if (cardTwo===cardFour) {
+//if (cardTwo===cardFour) {
 	alert.("You have found a match!")
-} else if (cardTwo===cardFour) {
+} else if (cardTwo!===cardFour) {
 	alert.("Sorry, try again.")
 }
+//
+
+document.getElementById('game-board');
+
+*/
+
+function createBoard() {
 
 
+	for (var i=0; i<4; i++) {
+	
+	var cardElement = document.createElement('div');
+	
+	cardElement.className = 'card';
+	
+	var board = document.getElementById("game-board");
+
+	board.appendChild(cardElement);
+
+	}
+}
+/*
+var createCards = function()
+
+
+
+function popup() {
+alert("Hello World")
+}
+*/
